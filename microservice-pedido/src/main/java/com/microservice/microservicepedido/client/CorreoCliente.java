59c5bcdb7d2adb555a238b38cab93e3d04c69639
Enzo Nina <ninaenzo70@gmail.com>
2024-11-16 17:12:31 -0500

    add pagos y notificaciones

--- microservice-pedido/src/main/java/com/microservice/microservicepedido/client/CorreoCliente.java
+++ microservice-pedido/src/main/java/com/microservice/microservicepedido/client/CorreoCliente.java
@@ -2,11 +2,13 @@ package com.microservice.microservicepedido.client;
 
 import com.microservice.microservicepedido.client.response.CorreoClientDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "msvc-correo", url = "correo-service:9031")
 public interface CorreoCliente {
 
+    @PostMapping("/enviar-correo/enviar")
     void enviarCorreo(@RequestBody CorreoClientDto dto);
 
 }
